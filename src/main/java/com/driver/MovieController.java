6c093dbec032ecf783905eadccb725263f0577cb
Rahul18018 <arahul18018@gmail.com>
2022-12-01 21:24:33 +0530

    updated

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("movies")
-public class MyController {
+public class MovieController {
     @Autowired
     MovieService movieService;
 
